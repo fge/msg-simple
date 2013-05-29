f3e201d1a4d220fbff53fc4908df355b204c9b83
Francis Galiegue <fgaliegue@gmail.com>
2013-05-29 15:02:22 +0200

    More tests to check MessageBundle logic

--- src/test/resources/com/github/fge/msgsimple/bundle/MessageBundleTest.java
+++ src/test/resources/com/github/fge/msgsimple/bundle/MessageBundleTest.java
@@ -30,4 +30,24 @@ public final class MessageBundleTest
         assertEquals(bundle.getKey(KEY1), KEY1);
         assertEquals(bundle.getKey(KEY2), KEY2);
     }
+
+    @Test
+    public void firstBundleToContainKeyWins()
+    {
+        final String msg1 = "foo";
+        final String msg2 = "bar";
+        when(source1.getMessage(KEY1)).thenReturn(msg1);
+        when(source2.getMessage(KEY2)).thenReturn(msg2);
+
+        final MessageBundle bundle = new MessageBundle.Builder()
+            .addSource(source1).addSource(source2).build();
+
+        assertEquals(bundle.getKey(KEY1), msg1);
+        verify(source1).getMessage(KEY1);
+        verify(source2, never()).getMessage(KEY1);
+
+        assertEquals(bundle.getKey(KEY2), msg2);
+        verify(source1).getMessage(KEY2);
+        verify(source2).getMessage(KEY2);
+    }
 }

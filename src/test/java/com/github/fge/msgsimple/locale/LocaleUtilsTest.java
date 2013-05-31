1a58f36ecaf1ffbbf8a11de936eedf237bb80ff1
Francis Galiegue <fgaliegue@gmail.com>
2013-05-31 20:33:06 +0200

    Tests for LocaleUtils.parse()
    There are some corner cases which fail, though. Namely, if the language is empty
    in an input string, Locale.ROOT is returned.

--- src/test/java/com/github/fge/msgsimple/locale/LocaleUtilsTest.java
+++ src/test/java/com/github/fge/msgsimple/locale/LocaleUtilsTest.java
@@ -1,6 +1,12 @@
 package com.github.fge.msgsimple.locale;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
 
 import static org.testng.Assert.*;
 
@@ -26,5 +32,53 @@ public final class LocaleUtilsTest
         } catch (IllegalArgumentException e) {
             assertEquals(e.getMessage(), "malformed input a_b_c_d");
         }
+    }
+
+    @DataProvider
+    public Iterator<Object[]> parsingData()
+    {
+        final List<Object[]> list = new ArrayList<Object[]>();
+
+        String input, language, country, variant;
+
+        input = "ja_JP_JP";
+        language = "ja";
+        country = "JP";
+        variant = "JP";
+        list.add(new Object[] { input, language, country, variant });
+
+        input = "fr_FR";
+        language = "fr";
+        country = "FR";
+        variant = "";
+        list.add(new Object[] { input, language, country, variant });
+
+        input = "it";
+        language = "it";
+        country = "";
+        variant = "";
+        list.add(new Object[] { input, language, country, variant });
+
+        input = "foo__bar";
+        language = "foo";
+        country = "";
+        variant = "bar";
+        list.add(new Object[] { input, language, country, variant });
+
+        input = language = country = variant = "";
+        list.add(new Object[] { input, language, country, variant });
+
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "parsingData")
+    public void localeParsingWorksCorrectly(final String input,
+        final String language, final String country, final String variant)
+    {
+        final Locale locale = LocaleUtils.parse(input);
+
+        assertEquals(locale.getLanguage(), language);
+        assertEquals(locale.getCountry(), country);
+        assertEquals(locale.getVariant(), variant);
     }
 }

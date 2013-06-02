dd0758ba7a5b5d71ab608b31fe613255c03bb20a
Francis Galiegue <fgaliegue@gmail.com>
2013-06-02 05:23:18 +0200

    UTF8ResouceBundle: skeleton implementation
    In fact, it should already be complete. But no tests are done yet.

---
+++ src/main/java/com/github/fge/msgsimple/bundle/UTF8ResouceBundle.java
@@ -0,0 +1,53 @@
+package com.github.fge.msgsimple.bundle;
+
+import com.github.fge.msgsimple.source.MessageSource;
+import com.github.fge.msgsimple.source.PropertiesMessageSource;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.regex.Pattern;
+
+/**
+ * A UTF-8 capable, {@link String}-only version of a {@link ResourceBundle}
+ */
+public final class UTF8ResouceBundle
+    extends CachedI18NMessageBundle
+{
+    private static final Pattern SUFFIX
+        = Pattern.compile("\\.properties$");
+
+    private final String prefix;
+
+    // FIXME: make this constructor package local and create a static factory
+    // method in I18NMessageBundle?
+    public UTF8ResouceBundle(final String basePath)
+    {
+        if (basePath == null)
+            throw new NullPointerException("base path must not be null");
+
+        prefix = SUFFIX.matcher(basePath).replaceFirst("");
+
+        try {
+            tryAndLookup(Locale.ROOT);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("there must at least be" +
+                " a properties file for Locale.ROOT; none was found", e);
+        }
+    }
+
+    @Override
+    protected MessageSource tryAndLookup(final Locale locale)
+        throws IOException
+    {
+        final StringBuilder sb = new StringBuilder(prefix);
+        final String localeString = locale.toString();
+
+        if (!localeString.isEmpty())
+            sb.append('_').append(localeString);
+
+        sb.append(".properties");
+
+        return PropertiesMessageSource.fromResource(sb.toString());
+    }
+}

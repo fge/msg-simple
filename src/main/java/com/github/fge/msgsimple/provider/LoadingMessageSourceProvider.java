cd42ae0ae5cc4b2fcc98669f078ec14489d028cc
Francis Galiegue <fgaliegue@gmail.com>
2013-06-05 11:31:18 +0200

    LoadingMessageSourceProvider: add concurrent access

--- src/main/java/com/github/fge/msgsimple/provider/LoadingMessageSourceProvider.java
+++ src/main/java/com/github/fge/msgsimple/provider/LoadingMessageSourceProvider.java
@@ -4,13 +4,26 @@ import com.github.fge.msgsimple.provider.load.MessageSourceLoader;
 import com.github.fge.msgsimple.source.MessageSource;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
 
 public final class LoadingMessageSourceProvider
     implements MessageSourceProvider
 {
+    private static final int NTHREADS = 5;
+
     private final MessageSourceLoader loader;
     private final MessageSource defaultSource;
+    private final ExecutorService service
+        = Executors.newFixedThreadPool(NTHREADS);
+    private final Map<Locale, FutureTask<MessageSource>> sources
+        = new HashMap<Locale, FutureTask<MessageSource>>();
 
     private LoadingMessageSourceProvider(final Builder builder)
     {
@@ -26,12 +39,39 @@ public final class LoadingMessageSourceProvider
     @Override
     public MessageSource getMessageSource(final Locale locale)
     {
+        FutureTask<MessageSource> task;
+
+        synchronized (sources) {
+            task = sources.get(locale);
+            if (task == null) {
+                task = loadingTask(locale);
+                sources.put(locale, task);
+                service.execute(task);
+            }
+        }
+
         try {
-            final MessageSource source = loader.load(locale);
+            final MessageSource source;
+            source = task.get();
             return source == null ? defaultSource : source;
-        } catch (IOException ignored) {
+        } catch (InterruptedException ignored) {
+            return defaultSource;
+        } catch (ExecutionException ignored) {
             return defaultSource;
         }
+    }
+
+    private FutureTask<MessageSource> loadingTask(final Locale locale)
+    {
+        return new FutureTask<MessageSource>(new Callable<MessageSource>()
+        {
+            @Override
+            public MessageSource call()
+                throws IOException
+            {
+                return loader.load(locale);
+            }
+        });
     }
 
     public static final class Builder
--- src/test/java/com/github/fge/msgsimple/provider/LoadingMessageSourceProviderTest.java
+++ src/test/java/com/github/fge/msgsimple/provider/LoadingMessageSourceProviderTest.java
@@ -6,7 +6,14 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static org.mockito.Mockito.*;
 import static org.testng.Assert.*;
@@ -16,6 +23,7 @@ public final class LoadingMessageSourceProviderTest
     private LoadingMessageSourceProvider.Builder builder;
     private MessageSourceLoader loader;
     private MessageSource defaultSource;
+    private MessageSource source;
 
     @BeforeMethod
     public void init()
@@ -23,6 +31,7 @@ public final class LoadingMessageSourceProviderTest
         builder = LoadingMessageSourceProvider.newBuilder();
         loader = mock(MessageSourceLoader.class);
         defaultSource = mock(MessageSource.class);
+        source = mock(MessageSource.class);
     }
 
     @Test
@@ -51,8 +60,7 @@ public final class LoadingMessageSourceProviderTest
     public void loaderIsUsedWhenItIsSet()
         throws IOException
     {
-        final MessageSourceProvider provider
-            = builder.setLoader(loader).build();
+        final MessageSourceProvider provider = builder.setLoader(loader).build();
 
         final Locale locale = Locale.ROOT;
 
@@ -90,11 +98,44 @@ public final class LoadingMessageSourceProviderTest
     public void defaultSourceIsReturnedIfLoaderThrowsAnException()
         throws IOException
     {
-        when(loader.load(any(Locale.class)))
-            .thenThrow(new IOException());
+        when(loader.load(any(Locale.class))).thenThrow(new IOException());
         final MessageSourceProvider provider = builder.setLoader(loader)
             .setDefaultSource(defaultSource).build();
 
         assertSame(provider.getMessageSource(Locale.ROOT), defaultSource);
+    }
+
+    @Test(dependsOnMethods = "loaderIsUsedWhenItIsSet")
+    public void loadingIsOnlyCalledOnce()
+        throws IOException, InterruptedException, ExecutionException
+    {
+        final Locale locale = Locale.ROOT;
+        when(loader.load(locale)).thenReturn(source);
+
+        final MessageSourceProvider provider = builder.setLoader(loader).build();
+
+        final int nThreads = 30;
+        final ExecutorService service = Executors.newFixedThreadPool(nThreads);
+        final List<Callable<MessageSource>> callables
+            = new ArrayList<Callable<MessageSource>>(nThreads);
+
+        for (int i = 0; i < nThreads; i++)
+            callables.add(new Callable<MessageSource>()
+            {
+                @Override
+                public MessageSource call()
+                    throws IOException
+                {
+                    return provider.getMessageSource(Locale.ROOT);
+                }
+            });
+
+        final List<Future<MessageSource>> results = service.invokeAll(callables);
+        service.shutdown();
+
+        for (int i = 0; i < nThreads; i++)
+            assertSame(results.get(i).get(), source);
+
+        verify(loader, only()).load(Locale.ROOT);
     }
 }

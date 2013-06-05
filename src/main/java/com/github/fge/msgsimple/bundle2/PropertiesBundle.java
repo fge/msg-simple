0da34354a61d056b8524e35238e1e1340d43359c
Francis Galiegue <fgaliegue@gmail.com>
2013-06-05 18:01:39 +0200

    PropertiesBundle: add javadoc

--- src/main/java/com/github/fge/msgsimple/bundle2/PropertiesBundle.java
+++ src/main/java/com/github/fge/msgsimple/bundle2/PropertiesBundle.java
@@ -8,8 +8,30 @@ import com.github.fge.msgsimple.source.PropertiesMessageSource;
 
 import java.io.IOException;
 import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
+/**
+ * Utility class to build a localized, UTF-8 {@link ResourceBundle}
+ *
+ * <p>This class only contains one method to return a {@link MessageBundle}
+ * which operates nearly the same as a {@link ResourceBundle}, with the
+ * difference that property files are read in UTF-8.</p>
+ *
+ * <p>This means that like a {@link ResourceBundle}, it will try and look up a
+ * key in locale-enabled property files (for instance, {@code
+ * foo_fr_FR.properties} for bundle {@code foo} and locale {@code fr_FR}).</p>
+ *
+ * <p>Internally, this method creates a dedicated {@link MessageSourceLoader} to
+ * load property files, wraps it into a {@link LoadingMessageSourceProvider},
+ * and finally creates a {@link MessageBundle} with this only source.</p>
+ *
+ * <p>As it is a {@link MessageBundle}, it means you can extend it further by
+ * {@link MessageBundle#thaw()}ing it and appending/prepending other message
+ * source providers etc.</p>
+ *
+ * @see PropertiesMessageSource
+ */
 public final class PropertiesBundle
 {
     private static final Pattern SUFFIX = Pattern.compile("\\.properties$");
@@ -18,6 +40,22 @@ public final class PropertiesBundle
     {
     }
 
+    /**
+     * Create a {@link ResourceBundle}-like {@link MessageBundle}
+     *
+     * <p>The path given as an argument can be any of the following:</p>
+     *
+     * <ul>
+     *     <li>{@code org/foobar/message.properties};</li>
+     *     <li>{@code org/foobar/message};</li>
+     *     <li>{@code /org/foobar/message.properties};</li>
+     *     <li>{@code /org/foobar/message}.</li>
+     * </ul>
+     *
+     * @param resourcePath the resource path
+     * @throws NullPointerException resource path is null
+     * @return a {@link MessageBundle}
+     */
     public static MessageBundle forPath(final String resourcePath)
     {
         if (resourcePath == null)

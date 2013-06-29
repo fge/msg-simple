95100066fb54b8ec2acab7fac195aea094a97b7e
Francis Galiegue <fgaliegue@gmail.com>
2013-06-29 14:09:32 +0200

    MessageBundlesTest: strenghten the test
    Count the number of invocations using an inner static class plus an
    AtomicInteger.

--- src/test/java/com/github/fge/msgsimple/load/MessageBundlesTest.java
+++ src/test/java/com/github/fge/msgsimple/load/MessageBundlesTest.java
@@ -3,10 +3,16 @@ package com.github.fge.msgsimple.load;
 import com.github.fge.msgsimple.bundle.MessageBundle;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.testng.Assert.*;
 
 public final class MessageBundlesTest
 {
+    private static final AtomicInteger COUNT = new AtomicInteger(0);
+    private static final MessageBundle BUNDLE
+        = MessageBundle.newBuilder().freeze();
+
     @Test
     public void bundleLoadOnlyHappensOnce()
     {
@@ -16,5 +22,19 @@ public final class MessageBundlesTest
             = MessageBundles.getBundle(DummyLoader.class);
 
         assertSame(bundle1, bundle2);
+        assertSame(bundle1, BUNDLE);
+        assertEquals(COUNT.get(), 1);
+    }
+
+    public static final class DummyLoader
+        implements MessageBundleLoader
+    {
+
+        @Override
+        public MessageBundle getBundle()
+        {
+            COUNT.incrementAndGet();
+            return BUNDLE;
+        }
     }
 }
